c911d2725cd06f4505d0027a4be6fe6e8333bc7b
Ben Noble <bennoble@bens-mbp.dhcp.wustl.edu>
2019-08-07 16:01:56 -0500

    Portfolio, Instrument Creation
    - Created Portfolio class
    - Create several cash functions
    - Created Financial Instrument Class

--- homeworks/hw1/hw1_ben.py
+++ homeworks/hw1/hw1_ben.py
@@ -7,3 +7,49 @@ Title: Homework 1
 Author: Ben Noble
 """
 
+# Creating the portfolio
+class Portfolio():
+  def __init__(self, name, cash):
+    self.name = name #client's name
+    self.cash = cash # client's starting cash value
+    self.stock = [] #empty stock list
+    self.mf = [] # empty mutual fund list
+  
+  def __str__(self):
+    return "%s has: \n Cash: $%s \n Stocks: %s \n Mutual Funds: %s" % (self.name, self.cash, self.stock, self.mf)
+
+  def check_bal(self):
+    return "%s has a balance of $%d" % (self.name, self.cash)
+  
+  def deposit(self, money):
+    self.cash += money
+    return self.check_bal()
+  
+  def withdraw(self, money):
+    if self.cash - money < 0:
+      return "Error. Insufficient Funds."
+    else:
+      self.cash -= money
+      return self.check_bal()
+  
+class FinancialInstruments():
+  def __init__(self, symbol, price):
+      self.symbol = symbol
+      self.price = price
+  
+  def __str__(self):
+    return "%s : %d" % (self.symbol, self.price)
+
+class Stock(FinancialInstrument):
+  
+class MututalFund(FinancialInstrument):
+    
+  
+bob = Portfolio("Bob", 50) #Creates a new portfolio
+print(bob)
+bob.check_bal()
+bob.deposit(80)
+bob.check_bal()
+bob.withdraw(40)
+bob.withdraw(100)
+bob.check_bal()
