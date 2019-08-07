6e3d4961a731edf1efd759a45534a64cb5bdc099
Ben Noble <bennoble@bens-mbp.dhcp.wustl.edu>
2019-08-07 16:32:14 -0500

    EOD 8/7 Update
    - Created stocks/mf classes to inherit financial instrument attributes
    - Revised stock/mf lists to dicts
    - Began buy stock function

--- homeworks/hw1/hw1_ben.py
+++ homeworks/hw1/hw1_ben.py
@@ -12,11 +12,11 @@ class Portfolio():
   def __init__(self, name, cash):
     self.name = name #client's name
     self.cash = cash # client's starting cash value
-    self.stock = [] #empty stock list
-    self.mf = [] # empty mutual fund list
+    self.stock_list = {"symbol": [], "buy price" : [], "shares" : []} #empty stock list
+    self.mf_list = {} # empty mutual fund list
   
   def __str__(self):
-    return "%s has: \n Cash: $%s \n Stocks: %s \n Mutual Funds: %s" % (self.name, self.cash, self.stock, self.mf)
+    return "%s has: \n Cash: $%s \n Stocks: %s \n Mutual Funds: %s" % (self.name, self.cash, self.stock_list, self.mf_list)
 
   def check_bal(self):
     return "%s has a balance of $%d" % (self.name, self.cash)
@@ -32,20 +32,41 @@ class Portfolio():
       self.cash -= money
       return self.check_bal()
   
-class FinancialInstruments():
-  def __init__(self, symbol, price):
-      self.symbol = symbol
+  def fin_withdraw(self, shares, stock):
+    if self.cash - stock.price * shares < 0:
+      return "Error. Insufficient Funds."
+    else:
+      self.cash -= stock.price * shares
+  
+  def buy_stock(self, shares, stock):
+    self.stock_list["shares"].append(shares)
+    self.stock_list["symbol"].append(stock.symbol)
+    self.stock_list["buy price"].append(stock.price)
+    #self.fin_withdraw() How does this take the withdrawl function to keep things DRY?
+
+  
+class FinancialInstrument():
+  def __init__(self, price, symbol):
       self.price = price
+      self.symbol = symbol
   
   def __str__(self):
     return "%s : %d" % (self.symbol, self.price)
 
 class Stock(FinancialInstrument):
-  
+  pass
+
 class MututalFund(FinancialInstrument):
-    
+  pass    
   
 bob = Portfolio("Bob", 50) #Creates a new portfolio
+s = Stock(20, "HFH")
+q = Stock(40, "ABC")
+print(s)
+bob.buy_stock(5, s)
+bob.buy_stock(2, q)
+print(bob)
+
 print(bob)
 bob.check_bal()
 bob.deposit(80)
