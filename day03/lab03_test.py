52a45d8f894c67a67bbc4c302ae6e9a3d6d33c99
Ben Noble <bennoble@bens-mbp.dhcp.wustl.edu>
2019-08-09 15:32:53 -0500

    Test Doc
    - Initializing test doc

--- day03/lab03_test.py
+++ day03/lab03_test.py
@@ -1,5 +1,5 @@
 import unittest
-from lab03 import *
+from lab03_ben import *
 
 class labTests(unittest.TestCase):
 	
@@ -7,14 +7,34 @@ class labTests(unittest.TestCase):
 	## make sure to account for correct and incorrect input
 
     def test_shout(self):
+      self.assertEqual("I AM A CAT", shout("I am a cat"))
+      with self.assertRaises(AttributeError): shout(5)
+      with self.assertRaises(AttributeError): shout([5, 6])
+      with self.assertRaises(TypeError): shout("I am a cat" + 12)
 
     def test_reverse(self):
-
+      self.assertEqual("tac a ma I", reverse("I am a cat"))
+      with self.assertRaises(AttributeError): shout(5)
+      with self.assertRaises(AttributeError): shout([5, 6])
+      with self.assertRaises(TypeError): shout("I am a cat" + 12)
+      
     def test_reversewords(self):
-
+      self.assertEqual("cat a am I", reversewords("I am a cat"))
+      with self.assertRaises(AttributeError): shout(5)
+      with self.assertRaises(AttributeError): shout([5, 6])
+      with self.assertRaises(TypeError): shout("I am a cat" + 12)
+      
     def test_reversewordletters(self):
+      self.assertEqual("I ma a tac", reversewordletters("I am a cat"))
+      with self.assertRaises(AttributeError): shout(5)
+      with self.assertRaises(AttributeError): shout([5, 6])
+      with self.assertRaises(TypeError): shout("I am a cat" + 12)      
 
     def test_piglatin(self):
+      self.assertEqual("arymay errytay", piglatin("Mary Terry"))
+      with self.assertRaises(AttributeError): shout(5)
+      with self.assertRaises(AttributeError): shout([5, 6])
+      with self.assertRaises(TypeError): shout("I am a cat" + 12)
 
 
 if __name__ == '__main__':
