d3c25a0db3b041524a063e74c53d11ac0e6c2f1d
Ben Noble <bennoble@bens-mbp.dhcp.wustl.edu>
2019-08-09 16:23:32 -0500

    Reset and Edits
    - Reverted to earlier version
    - Renamed some functions to conform with homework
    - Updated sell functions to work from strings

--- homeworks/hw1/hw1_ben.py
+++ homeworks/hw1/hw1_ben.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Aug  7 14:50:25 2019
-
 Title: Homework 1
 Author: Ben Noble
 """
@@ -23,23 +20,23 @@ class Portfolio():
   def __init__(self, name, cash):
     self.name = name #client's name
     self.cash = cash # client's starting cash value
-    self.stock_list = {"symbol": [], "shares" : []} #empty stock list
-    self.mf_list = {"symbol": [], "shares" : []} # empty mutual fund list
+    self.stock_list = {"symbol": [], "shares" : [], "stock" : []} #empty stock list
+    self.mf_list = {"symbol": [], "shares" : [], "mf" : []} # empty mutual fund list
     self.hist = []
 
 # Defining the print output of the portfolio object  
   def __str__(self):
-    return "%s has: \n Cash: $%.2f \n Stocks: %s \n Mutual Funds: %s" % (self.name, self.cash, self.stock_list, self.mf_list)
+    return "%s has: \n Cash: $%.2f \n Stocks: %s: %s \n Mutual Funds: %s: %s" % (self.name, self.cash, self.stock_list["symbol"], self.stock_list["shares"], self.mf_list["symbol"], self.mf_list["shares"])
 
 # Allows user to deposit money into their account  
-  def deposit(self, money):
+  def addCash(self, money):
     self.cash += money
     self.hist.append(time() + " Deposited $%d" % (money))
     print(self)
 
 # Allows user to withdraw money from their account. If they attempt to overdraw
 # the operation aborts.
-  def withdraw(self, money):
+  def withdrawCash(self, money):
     if self.cash - money < 0:
       return overdraft
     else:  
@@ -47,7 +44,7 @@ class Portfolio():
       self.hist.append(time() + " Withdrew $%d" % (money))
       print(self)
     
-  def buy_stock(self, shares, stock):
+  def buyStock(self, shares, stock):
 # Check to ensure stock is an integer value. If not, abort.
     if float(shares).is_integer() == False:
       return "Error. Stocks can only be purchased as whole units."
@@ -57,67 +54,68 @@ class Portfolio():
         return overdraft
 # Adds symbol and shares to the stock list, then takes cash from the account.
       else:
+        self.stock_list["stock"].append(stock)
         self.stock_list["shares"].append(shares)
         self.stock_list["symbol"].append(stock.symbol)
         self.cash -= stock.price * shares
-        self.hist.append(time() + " Purchased %d shares of %s" % (shares, stock.symbol))
+        self.hist.append(time() + " Purchased %d shares of %s for $%d each" % (shares, stock.symbol, stock.price))
       print(self)
 
-  def sell_stock(self, shares, stock):
-    if all(i != stock.symbol for i in self.stock_list["symbol"]):
-      return "Error. You do not own any shares of %s" % (stock.symbol)
-    for i in self.stock_list["symbol"]:
-      if i == stock.symbol:
-        ind = self.stock_list["symbol"].index(i)
-        if self.stock_list["shares"][ind] - shares < 0:
-          return overdraft
-        else:
-          self.stock_list["shares"][ind] = round(self.stock_list["shares"][ind] - shares, 2)
-          if self.stock_list["shares"][ind] == 0:
-            self.stock_list["shares"].pop(ind)
-            self.stock_list["symbol"].pop(ind)
-          sale_price = round(random.uniform(stock.price * .5, stock.price * 1.5), 2)
-          print("The sale price is %s per share" % (sale_price))
-          self.cash += sale_price * shares
+  def sellStock(self, symb, shares):
+    if symb not in self.stock_list["symbol"]:
+      return "Error. You do not own any shares of %s" % (symb)
+    elif symb in self.stock_list["symbol"]:
+      ind = self.stock_list["symbol"].index(symb)
+      stock =  self.stock_list["stock"][ind]
+      if self.stock_list["shares"][ind] - shares < 0:
+        return overdraft
+      else:
+        self.stock_list["shares"][ind] = round(self.stock_list["shares"][ind] - shares, 2)
+        if self.stock_list["shares"][ind] == 0:
+          self.stock_list["shares"].pop(ind)
+          self.stock_list["symbol"].pop(ind)
+        sale_price = round(random.uniform(stock.price * .5, stock.price * 1.5), 2)
+        print("The sale price is %s per share" % (sale_price))
+        self.cash += sale_price * shares
+        self.hist.append(time() + " Sold %d shares of %s for $%d each" % (shares, stock.symbol, sale_price))
       print(self)
   
-  def buy_mutual_fund(self, shares, mf):
+  def buyMutualFund(self, shares, mf):
 # Check to ensure sufficient funds to make the purchase. If not, abort.
     if self.cash - 1 * shares < 0:
       return overdraft
 # Adds symbol and shares to the mutual fund list, then takes cash from the account.
     else:
+      self.mf_list["mf"].append(mf)
       self.mf_list["shares"].append(shares)
       self.mf_list["symbol"].append(mf.symbol)
       self.cash -= 1 * shares
+      self.hist.append(time() + " Purchased %d shares of %s for $%d each" % (shares, mf.symbol, 1))
     print(self)
   
-  def sell_mutual_fund(self, shares, mf):
+  def sellMutualFund(self, symb, shares):
 # If the mutual fund selected is not in the mutual fund list, abort.
-    if all(i != mf.symbol for i in self.mf_list["symbol"]):
-      return "Error. You do not own any shares of %s." % (mf.symbol)
-# For the mutual fund selected that matches the mutual fund in the list...
-    for i in self.mf_list["symbol"]:
-      if i == mf.symbol:
-# capture the index value of that symbol in the mutual fund list.
-        ind = self.mf_list["symbol"].index(i)
-# Using that index, find that same index in shares. If user attempts to sell
-# more shares than they have, abort.
-        if self.mf_list["shares"][ind] - shares < 0:
+    if symb not in self.mf_list["symbol"]:
+      return "Error. You do not own any shares of %s" % (symb)
+    elif symb in self.mf_list["symbol"]:
+      ind = self.mf_list["symbol"].index(symb)
+# For the mutual fund selected that matches the mutual fund in the list....
+      if self.mf_list["shares"][ind] - shares < 0:
           return "Error. Insufficient shares."
-        else:
+      else:
 # Deduct the shares they want to sell from the mutual fund list
-          self.mf_list["shares"][ind] = round(self.mf_list["shares"][ind] - shares, 2)
+        self.mf_list["shares"][ind] = round(self.mf_list["shares"][ind] - shares, 2)
 # If they sell all their shares, remove the symbol and shares from their list
-          if self.mf_list["shares"][ind] == 0:
+        if self.mf_list["shares"][ind] == 0:
             self.mf_list["shares"].pop(ind)
             self.mf_list["symbol"].pop(ind)
 # Determine the sale price rounded to two digits
-          sale_price = round(random.uniform(0.9, 1.2), 2)
+        sale_price = round(random.uniform(0.9, 1.2), 2)
 # Tell the the user the sale price per share
-          print("The sale price is %s per share" % (sale_price))
+        print("The sale price is %s per share" % (sale_price))
 # Add the sale price * shares to their cash balance.
-          self.cash += sale_price * shares
+        self.cash += sale_price * shares
+        self.hist.append(time() + " Sold %d shares of %s for $%d each" % (shares, symb, sale_price))
     print(self)
 
   def history(self, num = None):
@@ -145,7 +143,7 @@ class MutualFund(FinancialInstrument):
   def __init__(self, symbol):
       FinancialInstrument.__init__(self, 1, symbol)   
       
-bob = Portfolio("Bob", 50) #Creates a new portfolio
+bob = Portfolio("Bob", 500) #Creates a new portfolio
 hfh = Stock(20, "HFH")
 abc = Stock(40, "ABC")
 brt = MutualFund("BRT")
@@ -155,19 +153,18 @@ print(hfh)
 print(brt)
 print(brt)
 
-bob.withdraw(600)
+bob.buyStock(3, abc)
+bob.sellStock("ABC", 1)
 
-
-bob.deposit(10)
-
-bob.buy_stock(1, abc)
 
 bob.history()
 
 bob.buy_stock(50, hfh)
-bob.sell_stock(1, hfh)
 
-bob.buy_mutual_fund(10.3, brt)
+bob.buyMutualFund(10.3, xyz)
+bob.sellMutualFund("XYZ", 5)
+
+
 bob.buy_mutual_fund(3.2, xyz)
 print(bob)
 
@@ -182,7 +179,6 @@ bob.sell_mutual_fund(2.2, xyz)
 
 #print(bob)
 #bob.check_bal()
-bob.deposit(80)
 #bob.check_bal()
 #bob.withdraw(40)
 #bob.withdraw(100)
