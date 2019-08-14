cddf4b5ab420a2c830875876564e096eacf553bc
Ben Noble <bennoble@bens-mbp.dhcp.wustl.edu>
2019-08-14 13:49:11 -0500

    Gov Approval Data Scraped!
    - Converted the governor text to a dictionary using json package
    - Created the csv writer to extract the relevant data and write rows

--- PosterProject/GovApprData.py
+++ PosterProject/GovApprData.py
@@ -1,57 +1,53 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 13 10:51:11 2019
+Governor Approval Data from Morning Consult
 
+Created on Tue Aug 13 10:51:11 2019
 @author: bennoble
 """
 
+# Preamble
 from bs4 import BeautifulSoup
 import urllib.request
 import csv 
-import unicodedata
-import re
+import json
 
+# Grabbing the morning consult governor rankings page 
 mc_web = "https://morningconsult.com/governor-rankings-q2-19/"
 mc_page = urllib.request.urlopen(mc_web)
-mc_page
 
+# Parsing the page
 parsed_page = BeautifulSoup(mc_page.read())
+
+# Finding the scripts
 scripts = parsed_page.find_all('script', {"type" : "text/javascript"})
 
+# Finding the specific script that contains the governor data
 for i in range(len(scripts)):
   for j in scripts[i]:
     if "var mc_senators" in j:
       app_data = scripts[i]
 
-for i in range(len(app_data)):
-  print(app_data[i])
-  
-print(app_data.values())
-app_data
+# Get the text of the governor text and convert it to a python dictionary
+app_text = app_data.get_text()
+pdict = json.loads(app_text[35:-12])
 
-baker = re.search('first_name(.+?)Q2"]}', app_data.get_text()).group()
-print(baker)
-
-pattern = re.compile('{"info":(.+?)Q2"]}')
-data = pattern.findall(app_data.get_text())
-data[0]
-
-fnames = []
-lnames = []
-for i in range(len(data)):
-  fnames.append(re.findall(r'"first_name":"(.+?)","last_name"', data[i]).pop()) 
-  lnames.append(re.findall(r'"last_name":"(.+?)","rank"', data[i]).pop())
-
-fnames
-lnames
-
-appr = []
-
-for i in range(len(data)):
-  appr.append(re.findall(r'"data":(.+?)},', data[i]).pop()) 
-
-appr
-
-re.findall(r'"data":(.+?)},', data[0])
-  
+# Writing the csv file
+with open('gov_approval_Q12017toQ219.csv', 'w') as f:
+  writer = csv.DictWriter(f, fieldnames = ('first name', 'last name', 'party', 'state', 'state abbr', 'quarter', 'approval', 'disapproval'))
+  writer.writeheader()
+  gov = {}
+# First subset by the dictionary keys (which are the governors full names)
+  for name in pdict.keys():
+# Then subset by each quarter we have for them
+    for q in range(len(pdict[name]['trend_dates'])):
+      gov['first name'] = pdict[name]["info"]["first_name"]
+      gov['last name'] = pdict[name]["info"]["last_name"]
+      gov['party'] = pdict[name]["info"]["party"]
+      gov['state'] = pdict[name]['info']['state']
+      gov['state abbr'] = pdict[name]['info']['state_abbr']
+      gov['quarter'] = pdict[name]['trend_dates'][q]
+      gov['approval'] = pdict[name]['trend_data'][0]['data'][q]
+      gov['disapproval'] = pdict[name]['trend_data'][1]['data'][q]
+      writer.writerow(gov)
