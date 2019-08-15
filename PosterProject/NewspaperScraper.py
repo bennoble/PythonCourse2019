26beeb005c2315eb57f8fceb4a69ec89b4965ae3
Ben Noble <bennoble@bens-mbp.dhcp.wustl.edu>
2019-08-15 14:44:22 -0500

    Finished scraping St. Louis Post
    - Scraped all STL post articles mentioning gov
    - created "test" csv so as to not overwrite my master csv file when testing

--- PosterProject/NewspaperScraper.py
+++ PosterProject/NewspaperScraper.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import csv 
 import unicodedata
-import datetime
+import time
 
 MO_searchURL = "https://www.stltoday.com/search/?f=html&q=mike+parson&d1=2018-04-01&d2=2019-07-01&s=start_time&sd=desc&l=100&t=article&nsa=eedition&app%5B0%5D=editorial&o=000"
 MO_search = urllib.request.urlopen(MO_searchURL)
@@ -22,13 +22,12 @@ h3 = parsed_MO.find_all('h3', {'class' : 'tnt-headline'})
 h3[2].find('a')['href']
 
 parsed_MO.find('time').get_text()
+article = urllib.request.urlopen("https://www.stltoday.com/opinion/editorial/editorial-nothing-says-pro-life-like-prosecuting-the-victim-for/article_e73e9589-d39f-5d1a-8294-4507c0380218.html")
+parsed_article = BeautifulSoup(article.read())
+parsed_article.find('time').get_text()
+parsed_article.find('div', {'itemprop' : 'articleBody'}).get_text().strip()
 
-day = datetime.datetime("Jul 1, 2019")
-print(day.strftime("%b"))
-datetime.strptime(day, "%m/%d/%y")
-
-datetime.datetime.strptime(day, "%b %d, %Y")
-
+# MO - St. Louis Post Dispatch
 with open('gov_newspapers.csv', 'w') as f:
   writer = csv.DictWriter(f, fieldnames = ('first name', 'last name', 'state', 'newspaper', 'article headline', 'date', 'url', 'body text'))
   writer.writeheader()
@@ -38,13 +37,22 @@ with open('gov_newspapers.csv', 'w') as f:
     mo_search = urllib.request.urlopen(mo_url)
     parsed_mo = BeautifulSoup(mo_search.read())
     h3_tag = parsed_mo.find_all('h3', {'class' : 'tnt-headline'})
-    for headline in range(2, len(h3_tag)):
-      news['first name'] = "Mike"
-      news['last name'] = 'Parsons'
-      news['state'] = 'MO'
-      news['newspaper'] = 'St. Louis Post Dispatch'
-      news['article headline'] = unicodedata.normalize('NFKD', h3_tag[headline].get_text()).strip()
-      news['url'] = 'https://www.stltoday.com' + h3_tag[headline].find('a')['href']
-      writer.writerow(news)
+    for headline in range(len(h3_tag)):
+      time.sleep(2)
+      try: 
+        news['first name'] = "Mike"
+        news['last name'] = 'Parsons'
+        news['state'] = 'MO'
+        news['newspaper'] = 'St. Louis Post Dispatch'
+        news['article headline'] = unicodedata.normalize('NFKD', h3_tag[headline].get_text()).strip()
+        news['url'] = 'https://www.stltoday.com' + h3_tag[headline].find('a')['href']
+        article = urllib.request.urlopen(news['url'])
+        parsed_article = BeautifulSoup(article.read())
+        news['date'] = parsed_article.find('time').get_text()
+        news['body text'] = parsed_article.find('div', {'itemprop' : 'articleBody'}).get_text().strip()
+        writer.writerow(news)
+      except urllib.error.HTTPError:
+        pass
+
     
   
