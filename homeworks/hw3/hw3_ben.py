abb18e9371d6ba01d7b6ac22f79a0fdd68ae73f1
Ben Noble <bennoble@Bens-MBP.attlocal.net>
2019-08-18 08:20:24 -0500

    Function Writing and Comments
    - Wrote functions that *should* answer all hw questions
    - Think my followers function is inefficient, so it's been running for a long time and is still working
    - New functions should be more efficient
    - Added robust comments

--- homeworks/hw3/hw3_ben.py
+++ homeworks/hw3/hw3_ben.py
@@ -20,13 +20,89 @@ api = twitter.client
 
 
 
+######################
+# 1A and 1B: Among the followers of @WUSTL, who has the greatest number of total tweets/followers
+######################
 
+def followers_comp():
+  tweets = 0
+  fols = 0
+  handle_tweets = None
+  handle_fols = None
+  i = 0
+# For each of @WUSTL's followers...
+  for fol in tweepy.Cursor(api.followers, id = 'WUSTL').items():
+# Increase the count; print every 1000th i—this just helps me keep track of the
+# the function's progress
+    i += 1
+    if i % 1000 == 0:
+      print(i)
+    try:
+# If the current followers follower count is greater than the existing count,
+# replace the existing count with the current count and save that user's name
+      if fol.followers_count > fols:
+        fols = fol.followers_count
+        handle_fols = fol.screen_name
+        print(handle_fols)
+# If the current followers tweet count is greater than the existing count,
+# replace the existing count with the current count and save that user's name
+      if fol.statuses_count > tweets:
+        tweets = fol.statuses_count
+        handle_tweets = fol.screen_name
+        print(handle_tweets)
+# May not need this since I set the API option to wait on rate limit
+    except tweepy.RateLimitError:
+      print("Rate Limit hit. Sleeping.")
+      time.sleep(15 * 60)
+      continue
+# Pause for 30s if there's a connection error
+    except ConnectionError:
+      print("Connection Error. Sleeping.")
+      time.sleep(30)
+      continue
+# Print the winner of each category
+  return "Most followers: %s %s. Most tweets: %s %s." % (handle_fols, fols, handle_tweets, tweets)
+
+q1 = followers_comp()  
+
+############################ delete?  
+
+follower_name = []
+follower_num = []
+follower_status_num = []
+      
+def followers():
+  for fol in tweepy.Cursor(api.followers, id = 'WUSTL').items():
+    try:
+# Get the name, number of followers, and number of tweets of each user 
+# @WUSTL follows and append to list
+      follower_name.append(fol.screen_name)
+      follower_num.append(fol.followers_count)
+      follower_status_num.append(fol.statuses_count)
+# I set the API to wait_on_rate_limit = True so this is probably not necessary
+# But I am paranoid!
+    except tweepy.RateLimitError:
+      print("Rate Limit hit. Sleeping.")
+      time.sleep(15 * 60)
+      continue
+# Pause for 30s if there's a connection error
+    except ConnectionError:
+      print("Connection Error. Sleeping.")
+      time.sleep(30)
+      continue
+# Call the followers function
+followers()  
+
+
+######################
+# 1C and 1D: Among those who @WUSTL follows, who has the greatest number of 
+# followers/tweets by category?
+######################
 
 follows_name = []
 follows_num = []
 follows_status_num = []
 
-# Capturing @WUSTL's follows
 def follows():
   for fol in tweepy.Cursor(api.friends, id = 'WUSTL').items():
     try:
@@ -44,7 +120,7 @@ def follows():
     except ConnectionError:
       time.sleep(30)
       continue
-# Call the followers function
+# Call the follows function
 follows()  
 
 len(follows_name) # Check to ensure we've captures all follows: 866
@@ -61,27 +137,123 @@ def follows_tweets():
   name_layman = None
   name_expert = None
   name_celeb = None
-  for i in range(len(follows_status_num)):
-    if follows_status_num[i]  < 100:
+# For each user I've already collected in the follows_num list...
+  for i in range(len(follows_num)):
+# If the current user has less than 100 followers, they are a layman
+    if follows_num[i] < 100:
+# If the current layman has more tweets than the existing layman, replace that
+# layman with the current layman and save their name
       if layman < follows_status_num[i]:
         layman = follows_status_num[i]
         name_layman = follows_name[i]
-      else:
-        pass
-    if follows_status_num[i] > 100 and follows_status_num[i] < 1000:
+# Elif the current user has between 100 and 1000 followers, they are an expert         
+    elif follows_num[i] > 100 and follows_num[i] < 1000:
+# If the current expert has more tweets than the existing expert, replace that
+# expert with the current expert and save their name
       if expert < follows_status_num[i]:
         expert = follows_status_num[i]
         name_expert = follows_name[i]
-    if follows_status_num[i] > 1000:
+# If the current user has more than 1000 followers, they are a celebrity
+    elif follows_num[i] > 1000:
+# If the current celebrity has more tweets than the existing celebrity, replace that
+# celebrity with the current celebrity and save their name
       if celeb < follows_status_num[i]:
         celeb = follows_status_num[i]
         name_celeb = follows_name[i]
-  print('%s: %s' % (name_layman, layman))
-  print('%s: %s' % (name_expert, expert))
-  print('%s: %s' % (name_celeb, celeb))
+# Print the winners in each category        
+  print('Layman: %s, %s' % (name_layman, layman))
+  print('Expert: %s, %s' % (name_expert, expert))
+  print('Celebrity: %s, %s' % (name_celeb, celeb))
       
 follows_tweets() # WUSTLdigital: 97 // CollegeCEOs: 994 // nytimes: 368674
-    
+
+######################
+# 2A: Among the (laymen and experts) followers of @WUSTLPoliSci and their 
+# followers, who has the greatest number of tweets?
+######################
+
+def followerfollower():
+  wustlps_followerfollower = []
+  count = 0
+  name = None
+  i = 0
+# For each follower of @WUSTLPoliSci...
+  for fol in tweepy.Cursor(api.followers, 'WUSTLPoliSci').items():
+# This part counts which user @WUSTLPoliSci follows that I am currently on, 
+# just helps me keep track of the function's progress since it takes a while to run
+    i += 1
+    print(i)
+    try:
+# I add the follower to the list then go through all the users followers
+# and append them to the list
+      wustlps_followerfollower.append(fol)
+      for folfol in tweepy.Cursor(api.followers, fol.screen_name).items():
+        wustlps_followerfollower.apend(folfol)
+# If there's a connection error, pause for 30s
+    except ConnectionError:
+      time.sleep(30)
+      continue
+# For each user on the list with less than 1000 followers... 
+  for user in wustlps_followerfollower:
+    if user.followers_count < 1000:
+# If the user's status count is greater than the current highest count, replace
+# the count and the screen name with this user
+      if user.statuses_count > count:
+        count = user.statuses_count
+        name = user.screen_name
+# Print the ultimate winner
+  return '%s, %s' % (name, count)
+
+q2a = followerfollower()
+q2a
+
+######################
+# 2B: Among those (laymen and experts) who @WUSTLPoliSci follows and those who 
+# they follow, who has the greatest number of tweets?
+######################
+
+def followfollow():
+  wustlps_followfollow = []
+  count = 0
+  name = None
+  i = 0
+# For each user @WUSTLPoliSci follows...
+  for fol in tweepy.Cursor(api.friends, 'WUSTLPoliSci').items():
+# This part counts which user @WUSTLPoliSci follows that I am currently on, 
+# just helps me keep track of the function's progress since it takes a while to run
+    i += 1
+    print(i)
+    try:
+# I add the followed user to the list then go through all the users they're
+# following and append them to the list
+      wustlps_followfollow.append(fol)
+      for folfol in tweepy.Cursor(api.friends, fol.screen_name).items():
+        wustlps_followfollow.append(folfol)
+# If there's a connection error, pause for 30s
+    except ConnectionError:
+      time.sleep(30)
+      continue
+# For each user on the list with less than 1000 followers... 
+  for user in wustlps_followfollow:
+    if user.followers_count < 1000:
+# If the user's status count is greater than the current highest count, replace
+# the count and the screen name with this user
+      if user.statuses_count > count:
+        count = user.statuses_count
+        name = user.screen_name
+# Print the ultimate winner
+  return '%s, %s' % (name, count)
+
+q2b = followfollow()
+q2b
+
+
+
+################### Delete
+
+fol_name1 = follower_name
+fol_num1 = follower_num
+fol_stat1 = follower_status_num
     
 laymen['tweet num'].append(follows_status_num[i])
 laymen['name'].append(follows_name[i])
@@ -167,17 +339,8 @@ follows_name[follows_num.index(max(follows_num))]
 follows_name1 = follows_name
 len(follows_name)
 
-for fol in tweepy.Cursor(api.friends, id = 'WUSTL').items(5):
-    while True:  
-      try:
-        #api = twitter.client
-        print(fol.screen_name)
-      except tweepy.RateLimitError:
-        print("no")
-      except tweepy.TweepError:
-        print("tweep")
-
-  
+for fol in tweepy.Cursor(api.friends, id = 'WUSTL').items(1):
+   print(fol)
   
 for i in range(len(follows_name)):
   print('name %s, num fol %s, num status %s' % (follows_name[i], follows_num[i], follows_status_num[i]))
@@ -204,3 +367,48 @@ for fol in tweepy.Cursor(api.friends, id = 'WUSTL').items():
   follows_num.append(fol.followers_count)
   follows_status_num.append(fol.statuses_count)
 
+test = [1, 4, 7, 9, 2, 3]
+ind = None
+a = 0
+for i in range(len(test)):
+  if test[i] < 8:
+    if test[i] > a:
+      a = test[i]
+      ind = i
+print(a)
+print(ind)  
+
+for item in tweepy.Cursor(api.followers, 'realDonaldTrump').items(2):
+	print(item)
+    
+wustlps = api.get_user('WUSTLPoliSci')
+wustlps.followers
+wustlps.statuses_count
+wustlps.screen_name
+dir(wustlps)
+
+def followfollow():
+  wustlps_followfollow = []
+  count = 0
+  name = None
+  for fol in tweepy.Cursor(api.friends, 'WUSTLPoliSci').items(2):
+    wustlps_followfollow.append(fol)
+    for folfol in tweepy.Cursor(api.friends, fol.screen_name).items():
+      wustlps_followfollow.append(folfol)
+  for user in wustlps_followfollow:
+    if user.followers_count < 1000:
+      print(user.statuses_count)
+      print(user.screen_name)
+      if user.statuses_count > count:
+        count = user.statuses_count
+        name = user.screen_name
+  return '%s, %s' % (name, count)
+
+followfollow()
+
+
+dt_fols = []
+for fol in tweepy.Cursor(api.friends, dt.screen_name).items(1):
+  dt_fols.append(fol)
+
+dt_fols[0].statuses_count
